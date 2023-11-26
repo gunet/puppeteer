01f3994598b7a7ab72c4324695325e20b5865905
kkalev <kkalev@gmail.com>
2023-11-26 10:37:52 +0200

    fix typo

--- scenarios/cas/login.js
+++ scenarios/cas/login.js
@@ -8,7 +8,7 @@ console.log(url[0]);
     const page = await gunet.newPage(browser);
     await page.goto(url[0]);
     await page.waitForTimeout(1000)
-    await gunet.loginWith(page, `${process.env.gunet_USER}`, `${process.env.gunet_PASSWORD}`);
+    await gunet.loginWith(page, `${process.env.CAS_USER}`, `${process.env.CAS_PASSWORD}`);
     await gunet.assertTicketGrantingCookie(page);
     await page.waitForTimeout(2000)
     await gunet.assertInnerText(page, '#content div h2', "Επιτυχής Σύνδεση");
