0d47af4c4788fd7285f278371168392f9e299354
kkalev <kkalev@gmail.com>
2023-11-26 13:21:23 +0200

    add support for simple-cas

--- README.md
+++ README.md
@@ -51,6 +51,9 @@ console.log(url[0]);
 * Environment variables
   - `CAS_USER`: Username (default `gunetdemo`)
   - `CAS_PASSWORD`: Password (default `gunetdemo`)
+  - `CAS_TYPE`: The CAS type. Can be one of:
+    * `cas`: If using Apereo CAS (the default)
+    * `simple-cas`: If using the gunet/simpleidentity `simple-cas` Docker container   
 * Show module version: `docker run --rm ghcr.io/gunet/puppeteer npm list`
 * General Run: `docker run -it --cap-add=SYS_ADMIN --rm ghcr.io/gunet/puppeteer node --unhandled-rejections=strict <scenario + args>`
 * Specific scenarios:
--- login.js
+++ login.js
@@ -11,6 +11,11 @@ console.log(url[0]);
     await gunet.loginWith(page, `${process.env.CAS_USER}`, `${process.env.CAS_PASSWORD}`);
     await gunet.assertTicketGrantingCookie(page);
     await page.waitForTimeout(2000)
-    await gunet.assertInnerText(page, '#content div h2', "Επιτυχής Σύνδεση");
+    if (process.env.CAS_TYPE === 'cas') {
+        await gunet.assertInnerText(page, '#content div h2', "Επιτυχής Σύνδεση");
+    }
+    else if (process.env.CAS_TYPE === 'simple-cas') {
+        await gunet.assertInnerText(page, '#content div h2', "Log In Successful");
+    }
     await browser.close();
 })();
